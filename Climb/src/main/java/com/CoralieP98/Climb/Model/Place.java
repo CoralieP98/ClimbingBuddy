17cb8224190afdab64914d6e8316549df837a149
coralieP98 <coralie.perez98@orange.fr>
2024-07-25 00:11:28 +0200

    docker ok! some model added

--- Climb/src/main/java/com/CoralieP98/Climb/Model/Place.java
+++ Climb/src/main/java/com/CoralieP98/Climb/Model/Place.java
@@ -1,9 +1,42 @@
 package com.CoralieP98.Climb.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int placeId;
+
+    private String labelPlace;
+
+    public Place() {
+    }
+
+    public Place(int placeId, String labelPlace) {
+        this.placeId = placeId;
+        this.labelPlace = labelPlace;
+    }
+
+    public int getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(int placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getLabelPlace() {
+        return labelPlace;
+    }
+
+    public void setLabelPlace(String labelPlace) {
+        this.labelPlace = labelPlace;
+    }
 }
--- Climb/src/main/java/com/CoralieP98/Climb/Model/Route.java
+++ Climb/src/main/java/com/CoralieP98/Climb/Model/Route.java
@@ -1,4 +1,29 @@
 package com.CoralieP98.Climb.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int RouteId;
+
+//    private int typeId;
+
+    private boolean isSlab;
+
+    private boolean isOverHang;
+
+    private boolean isLead;
+
+    private boolean isTopRope;
+
+    private float lenght;
+
 }
