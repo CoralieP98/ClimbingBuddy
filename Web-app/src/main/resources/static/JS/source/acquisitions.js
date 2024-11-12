8cce67f855c3235be8d4634c275a96410dcde2b6
cocomeh <coralie.perez98@orange.fr>
2024-11-12 17:02:58 +0100

    stats pour prez done

--- Web-app/src/main/resources/static/JS/source/acquisitions.js
+++ Web-app/src/main/resources/static/JS/source/acquisitions.js
@@ -9,7 +9,8 @@ const charData = {
     datasets : [{
         label : "nbTech",
         data: data.map(row => row.technique_count),
-        // backgroundColor:["blue","cyan","navy","royalblue","indigo","lightblue"]
+        backgroundColor:["blue","cyan","navy","royalblue","indigo","lightblue"],
+        color : "white"
     }]
 }
 
@@ -28,32 +29,3 @@ const myChart = new Chart(ctx,{
 
 
 })
-//
-// (async function () {
-//     window.addEventListener('load', function () {
-//
-//         const ctx = document.getElementById('nbTechniques');
-//         console.log("PWEEEET")
-//
-//         let data = JSON.parse(document.querySelector('#jsonData').outerText);
-//         new Chart(ctx, {
-//             type: 'pie',
-//             data: {
-//                 labels: data.map(row => row.technique_name),
-//                 datasets: [{
-//                     label: 'Part of product in vending',
-//                     data: data.map(row => row.technique_count),
-//                 }]
-//             },
-//             // options: {
-//             //     responsive: false,
-//             //     style: false,
-//             //     plugins: {
-//             //         legend: {
-//             //             position: 'right',
-//             //         }
-//             //     }
-//             // }
-//         });
-//     });
-// })();
